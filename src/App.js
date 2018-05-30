8c340e761d46729b00ed952b1f8e0395202b6579
Kenny Howard <khoward@overstock.com>
2018-05-30 13:22:28 -0600

    prepping to implement firebase

--- src/App.js
+++ src/App.js
@@ -1,13 +1,14 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import Note from './Note/Note';
+import NoteForm from './NoteForm/NoteForm';
 import './App.css';
 
 class App extends Component {
 	constructor(props) {
 		super(props);
+		this.updateNoteList = this.updateNoteList.bind(this)
 
-		// going to setup the React state of our component
 		this.state = {
 			notes: [
 				{ id: 1, noteContent: 'Note 1 here' },
@@ -15,6 +16,15 @@ class App extends Component {
 			]
 		};
 	}
+
+	updateNoteList(note) {
+		const previousNotes = this.state.notes;
+		previousNotes.push({ id: previousNotes.length + 1, noteContent: note });
+		this.setState({
+			notes: previousNotes
+		})
+	}
+
 	render() {
 		return (
 			<div className="notes-wrapper">
@@ -22,16 +32,18 @@ class App extends Component {
 					<div className="heading">My Dope To Do List</div>
 				</div>
 				<div className="notes-body">
-					{
-						this.state.notes.map(function(note) {
-							return (
-								<Note noteContent={note.noteContent} noteId={note.Id} key={note.Id} />
-							)
-						})
-					}
+					{this.state.notes.map(function(note) {
+						return (
+							<Note
+								noteContent={note.noteContent}
+								noteId={note.Id}
+								key={note.Id}
+							/>
+						);
+					})}
 				</div>
 				<div className="notes-footer">
-					Footer will go here...
+					<NoteForm updateNoteList={this.updateNoteList} />
 				</div>
 			</div>
 		);
---
+++ src/NoteForm/NoteForm.jsx
@@ -0,0 +1,46 @@
+import React, { Component } from 'react';
+import './NoteForm.css';
+
+class NoteForm extends Component {
+	constructor(props) {
+		super(props);
+		this.state = {
+			newNoteContent: ''
+		};
+		// we need to explicitly bind our input
+		this.handleUserInput = this.handleUserInput.bind(this);
+		this.addNote = this.addNote.bind(this);
+	}
+
+	handleUserInput(e) {
+		this.setState({
+			newNoteContent: e.target.value
+		});
+	}
+
+	addNote() {
+		this.props.updateNoteList(this.state.newNoteContent);
+
+		this.setState({
+			newNoteContent: '',
+		})
+	}
+
+	render() {
+		return (
+			<div className="form-wrapper">
+				<input
+					className="note-input"
+					placeholder="Write a new note..."
+					value={this.state.newNoteContent}
+					onChange={this.handleUserInput}
+				/>
+				<button className="note-button" onClick={this.addNote}>
+					Add Note
+				</button>
+			</div>
+		);
+	}
+}
+
+export default NoteForm;
